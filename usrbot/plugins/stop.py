0a50288289af9261762784360cd495613cedec2d
yurii <uraklimek@gmail.com>
2025-09-08 13:35:23 +0300

    add stop

---
+++ usrbot/plugins/stop.py
@@ -0,0 +1,22 @@
+# __command__ = ".restart"
+# __info__ = "Restarts the bot"
+
+from pyrogram import filters
+from ..usrbot import UsrBot
+import os
+import logging
+log = logging.getLogger(__name__)
+
+
+
+
+@UsrBot.on_message(filters.command("stop", prefixes="."))
+async def stop(_, message):
+    log.info("help command received")
+
+    await message.reply_text("Stopping...")
+    log.info("Stopping...")
+
+    os.system("bash stop")
+
+
